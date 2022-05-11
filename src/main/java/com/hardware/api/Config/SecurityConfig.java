5cccca5666d9a071bbf61c354322a90e626c5e5a
Lucas Celestino <lucasrashido@gmail.com>
2022-05-11 16:33:31 -0300

    configurações adicionais

--- src/main/java/com/hardware/api/Config/SecurityConfig.java
+++ src/main/java/com/hardware/api/Config/SecurityConfig.java
@@ -1,9 +1,15 @@
 package com.hardware.api.Config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -16,8 +22,19 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().antMatchers(PUBLIC_MATCHERS)
+        http.cors().and().csrf().disable();
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.authorizeRequests().antMatchers(HttpMethod.GET, PUBLIC_MATCHERS)
         .permitAll().anyRequest().authenticated();
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+    final UrlBasedCorsConfigurationSource source = new
+    UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", new
+    CorsConfiguration().applyPermitDefaultValues());
+    return source;
     }
 
 }

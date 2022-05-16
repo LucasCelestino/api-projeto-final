48ee72bddcfa018ff6de5cc12509ef35f1eaa6a2
Lucas Celestino <lucasrashido@gmail.com>
2022-05-16 15:10:04 -0300

    configurações JWT no SecurityConfig

--- src/main/java/com/hardware/api/Config/SecurityConfig.java
+++ src/main/java/com/hardware/api/Config/SecurityConfig.java
@@ -1,8 +1,5 @@
 package com.hardware.api.Config;
 
-import com.hardware.api.Security.JWTAuthenticationFilter;
-import com.hardware.api.Security.JWTUtil;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,11 +14,19 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import com.hardware.api.Security.JWTAuthenticationFilter;
+import com.hardware.api.Security.JWTUtil;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter
 {
+    @Autowired
+    private JWTUtil jwtUtil;
+    
+    @Autowired
+    private UserDetailsService userDetailsService;
+
     private static final String[] PUBLIC_MATCHERS = {
         "/api/v1/users/**",
         "/api/v1/parts/**",
@@ -35,13 +40,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter
         "/api/v1/brands/**",
         "/api/v1/budgets/**"
     };
-
-    @Autowired
-    private JWTUtil jwtUtil;
-    
-    @Autowired
-    private UserDetailsService userDetailsService;
-       
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
